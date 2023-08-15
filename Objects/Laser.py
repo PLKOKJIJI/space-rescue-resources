4dafafe9ca0bed3b26e2f080256d6fd8afa8968c
PLKOKJIJI <139776434+PLKOKJIJI@users.noreply.github.com>
2023-08-15 14:09:48 +1000

    Laser Asteroid

--- Objects/Laser.py
+++ Objects/Laser.py
@@ -18,6 +18,9 @@ class Laser(RoomObject):
         
         # set movement
         self.set_direction(0, 50)
+
+        # handle events
+        self.register_collision_object("Asteroid")
         
     def step(self):
         """
@@ -31,3 +34,11 @@ class Laser(RoomObject):
         """
         if self.x > Globals.SCREEN_WIDTH:
             self.room.delete_object(self)
+
+    # --- Events handlers
+    def handle_collision(self, other, other_type):
+        """
+        handles laser collisions with other registered objects
+        """
+        if other_type == "Asteroid":
+            self.room.delete_object(other)
